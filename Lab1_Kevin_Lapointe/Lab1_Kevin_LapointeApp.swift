21c57a26ac4ff9a90578b897b05dea1428553473
Lapointek <156107790+lapointek@users.noreply.github.com>
2025-02-08 13:07:56 -0500

    Initial Commit

---
+++ Lab1_Kevin_Lapointe/Lab1_Kevin_LapointeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Kevin_LapointeApp.swift
+//  Lab1_Kevin_Lapointe
+//
+//  Created by Kevin Lapointe on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Kevin_LapointeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
